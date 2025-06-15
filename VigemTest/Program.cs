03b39850f5ea75a8f89efc9b108420baeac21342
Leeway001Write <76715191+Leeway001Write@users.noreply.github.com>
2025-06-15 00:07:45 -0600

    See last commit; forgot to add VigemController file to git. Still learning how to use git from the command line.

---
+++ VigemTest/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using Nefarius.ViGEm.Client;
+using Nefarius.ViGEm.Client.Targets;
+using Nefarius.ViGEm.Client.Targets.Xbox360;
+
+class Program
+{
+    static void Main()
+    {
+        using var client = new ViGEmClient();
+
+        IXbox360Controller controller = client.CreateXbox360Controller();
+        controller.Connect();
+
+        Console.WriteLine("Xbox 360 Controller connected!");
+
+        while (true)
+        {
+            controller.SetButtonState(Xbox360Button.RightShoulder, true);
+            controller.SetButtonState(Xbox360Button.A, true);
+            Thread.Sleep(500);
+
+            controller.SetButtonState(Xbox360Button.RightShoulder, false);
+            controller.SetButtonState(Xbox360Button.A, false);
+            Thread.Sleep(500);
+        }
+    }
+}
